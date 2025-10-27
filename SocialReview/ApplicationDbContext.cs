1b4f454b32b8eb99e949f80d46862412928a388f
thaiph <thaiph@DESKTOP-6PS4JU0>
2025-10-27 16:16:29 +0700

    Add project files.

---
+++ SocialReview/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SocialReview
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+        
+        
+        }
+        //DbSet<>
+    }
+}
